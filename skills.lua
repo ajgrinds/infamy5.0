076266540887bfbfa9f8ee635b17bf4634173f5c
ajgrinds <ajgrinds03@gmail.com>
2021-05-06 00:43:20 -0700

    I agree

--- skills.lua
+++ skills.lua
@@ -3,3 +3,9 @@ function(self)
   self:_respec_tree_version5(1, 1)
   self:_aquire_points(1000, true)
 end)
+
+Hooks:PostHook(SkillTreeManager, "on_respec_tree", "tester",
+function(tree, forced_respec_multiplier)
+  log(tree)
+  log(forced_respec_multiplier)
+end)
